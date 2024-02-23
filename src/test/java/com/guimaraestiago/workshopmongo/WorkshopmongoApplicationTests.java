b61ad51153b1b684c50d0adc3123c11157f621b8
Tiago Guimarães <tiago_guimaraesq@hotmail.com>
2024-02-23 12:39:53 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/guimaraestiago/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guimaraestiago.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
