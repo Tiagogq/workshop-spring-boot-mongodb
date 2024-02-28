c509dcbb048385c5889469ab5253833fde61d27d
Tiago Guimarães <tiago_guimaraesq@hotmail.com>
2024-02-28 12:23:17 -0300

    Obtendo um usuário por id

--- src/main/java/com/guimaraestiago/workshopmongo/services/UserService.java
+++ src/main/java/com/guimaraestiago/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.guimaraestiago.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guimaraestiago.workshopmongo.domain.User;
 import com.guimaraestiago.workshopmongo.repository.UserRepository;
+import com.guimaraestiago.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();	
+	}
+	
+	public User findbyId(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/com/guimaraestiago/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.guimaraestiago.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
